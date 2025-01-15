7e8bd647f7819ef9621724b8555f4815a45eecbb
Denis_Gershovich <denis.gershovich@gmail.com>
2025-01-15 13:30:44 +0200

    Create file for constants

---
+++ back-end/src/constants.ts
@@ -0,0 +1,22 @@
+const pizzaNames = [
+  "Margherita Magic",
+  "Pepperoni Paradise",
+  "Veggie Delight",
+  "Supreme Slice",
+  "Cheese Lovers' Dream",
+  "Mamma's Meat Feast",
+  "Spicy Inferno",
+  "BBQ Chicken Bonanza",
+  "Four Seasons Special",
+  "Tuscan Delight",
+];
+
+const pizzaTypes = [
+  "Margherita",
+  "Pepperoni",
+  "Veggie",
+  "Hawaiian",
+  "BBQ Chicken",
+];
+
+export { pizzaNames, pizzaTypes };
